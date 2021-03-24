35ff655f599e2c7a789288d0cb1d261566770506
Yadunandan Pillai <ytpillai@pm.me>
2021-03-23 21:15:44 -0400

    Stuff
    Signed-off-by: Yadunandan Pillai <ytpillai@pm.me>

---
+++ target/arm-unknown-linux-gnueabihf/debug/deps/serial_core-50aff8b7e8f70620.d
@@ -0,0 +1,7 @@
+/home/diablo/docs/college/fall2020/pulse/repo/src/websockets/rust_ws_server/target/arm-unknown-linux-gnueabihf/debug/deps/serial_core-50aff8b7e8f70620.rmeta: /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/serial-core-0.4.0/src/lib.rs
+
+/home/diablo/docs/college/fall2020/pulse/repo/src/websockets/rust_ws_server/target/arm-unknown-linux-gnueabihf/debug/deps/libserial_core-50aff8b7e8f70620.rlib: /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/serial-core-0.4.0/src/lib.rs
+
+/home/diablo/docs/college/fall2020/pulse/repo/src/websockets/rust_ws_server/target/arm-unknown-linux-gnueabihf/debug/deps/serial_core-50aff8b7e8f70620.d: /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/serial-core-0.4.0/src/lib.rs
+
+/home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/serial-core-0.4.0/src/lib.rs:
---
+++ target/arm-unknown-linux-gnueabihf/debug/deps/termios-e1d04ea2de4690d0.d
@@ -0,0 +1,10 @@
+/home/diablo/docs/college/fall2020/pulse/repo/src/websockets/rust_ws_server/target/arm-unknown-linux-gnueabihf/debug/deps/termios-e1d04ea2de4690d0.rmeta: /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/lib.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/ffi.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/mod.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/linux.rs
+
+/home/diablo/docs/college/fall2020/pulse/repo/src/websockets/rust_ws_server/target/arm-unknown-linux-gnueabihf/debug/deps/libtermios-e1d04ea2de4690d0.rlib: /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/lib.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/ffi.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/mod.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/linux.rs
+
+/home/diablo/docs/college/fall2020/pulse/repo/src/websockets/rust_ws_server/target/arm-unknown-linux-gnueabihf/debug/deps/termios-e1d04ea2de4690d0.d: /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/lib.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/ffi.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/mod.rs /home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/linux.rs
+
+/home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/lib.rs:
+/home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/ffi.rs:
+/home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/mod.rs:
+/home/diablo/.cargo/registry/src/github.com-1ecc6299db9ec823/termios-0.2.2/src/os/linux.rs:
