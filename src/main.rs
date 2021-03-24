a820d38b31774cdc452e2f5fc5072e534f32e4a2
Yadunandan Pillai <ytpillai@pm.me>
2021-03-23 22:39:56 -0400

    ASD
    Signed-off-by: Yadunandan Pillai <ytpillai@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,5 @@ use std::time::Duration;
 fn main() {
     let mut uart = Uart::with_path("/dev/ttyAMA0", 9600, Parity::Even, 8, 2).expect("COULD SET UART");
     uart.set_read_mode(1, Duration::default()).expect("COULD SET UART MODE");
+    println!("ASD");
 }
